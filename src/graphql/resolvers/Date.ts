5e61df38d2c37d3d0c007639252fecfd774ba0fd
Manuel Ruck <git@manuelruck.de>
2025-11-09 09:32:47 +0100

    chore(graphql): migrate to Apollo Server v4 and express4 integration
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/graphql/resolvers/Date.ts
+++ src/graphql/resolvers/Date.ts
@@ -1,8 +1,8 @@
-import GraphQLDate from 'graphql-date';
+import { DateTimeResolver } from 'graphql-scalars';
 import { Resolvers } from '../../generated/graphql';
 
 const DateApi: Resolvers = {
-  Date: GraphQLDate,
+  Date: DateTimeResolver,
 };
 
 export default DateApi;
